c52a276f37405c3ecaf6a26615e8ed6b983350df
Jairo <calderonjairo666@gmail.com>
2026-05-07 21:42:35 -0500

    feat(enemies): add 2D pixel art skeleton with directional animations

---
+++ Assets/Scripts/SkeletonAnimationEvents.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class SkeletonAnimationEvents : MonoBehaviour
+{
+     private EnemyMovement enemyMovement;
+
+    void Start()
+    {
+        enemyMovement = GetComponentInParent<EnemyMovement>();
+    }
+
+    public void DealDamage()
+    {
+        if (enemyMovement != null)
+            enemyMovement.DealDamage();
+    }
+    
+}
